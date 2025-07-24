0c66dcaf2516b9075e02f583b8382a234cf6d0c0
ALRayan <alrayanrashid043@gmail.com>
2025-07-24 10:06:52 +0400

    Add solution for warmup1-diff21

---
+++ diff21.java
@@ -0,0 +1,19 @@
+
+class diff21 {
+    public static int diff21(int n) {
+        if (n > 21){
+            return 2 * (n - 21);
+        } else {
+            return 21 - n ;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(diff21(19)); // 2
+        System.out.println(diff21(25)); // 8
+    }
+}
+
+
+
+
